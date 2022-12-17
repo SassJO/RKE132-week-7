96730dcf511021beb637987f399df2883f0a2c21
SassJO <sassjorgen@gmail.com>
2022-12-17 12:58:27 +0200

    week7

---
+++ week7functions/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Are you coming or leaving?(in/out)");
+string userChoise = Console.ReadLine();
+
+if(userChoise == "in")
+{
+    PrintHello();
+
+}
+else
+{
+    PrintGoodBye();
+}
+
+PrintHello();
+PrintGoodBye();
+
+static void PrintHello()
+{
+    Console.WriteLine("hello world!");
+}
+static void PrintGoodBye()
+{
+    Console.WriteLine("see you later");
+}
+
+
+
